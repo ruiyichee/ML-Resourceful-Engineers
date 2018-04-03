c7ad1a502efed8187fee7847819a68c8719bdb6a
Rui\ruiyicx <ruiyi_chee@mymail.sutd.edu.sg>
2018-04-03 21:42:03 +0800

    Updated ML part 2 code

--- ML_Proj_Part2.py
+++ ML_Proj_Part2.py
@@ -1,5 +1,5 @@
 # Set Working Directory to your source file; insert double \
-import os
+#import os
 # General Resourceful Engineers File
 #os.chdir("C:\\Users\\ruiyicx\\Documents\\SUTD Subjects\\ESD Term 7\\01.112 Machine Learning\\Project")
 
@@ -7,10 +7,10 @@ import os
 #os.chdir("C:\\Users\\ruiyicx\\Documents\\SUTD Subjects\\ESD Term 7\\01.112 Machine Learning\\Project\\CN\\CN")
 
 def get_data(data):
-	raw_data = open(data, "r+", encoding="utf-8")	# r+ is Special read and write mode, which is used to handle both actions when working with a file
+	raw_data = open(data, 'r', encoding= "utf8")	# r+ is Special read and write mode, which is used to handle both actions when working with a file
 	read = raw_data.read()
 	print (read)
 
-directory1 = "C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/dev.in"
+directory1 = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/train'
 
 get_data(directory1)
