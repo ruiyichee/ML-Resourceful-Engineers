d0bfd706fea902b63d2cb8e29589856d8ec24247
Rui\ruiyicx <ruiyi_chee@mymail.sutd.edu.sg>
2018-04-03 16:05:28 +0800

    Created Python File for Part 2 Code

---
+++ ML_Proj_Part2.py
@@ -0,0 +1,16 @@
+# Set Working Directory to your source file; insert double \
+import os
+# General Resourceful Engineers File
+#os.chdir("C:\\Users\\ruiyicx\\Documents\\SUTD Subjects\\ESD Term 7\\01.112 Machine Learning\\Project")
+
+# CN File
+#os.chdir("C:\\Users\\ruiyicx\\Documents\\SUTD Subjects\\ESD Term 7\\01.112 Machine Learning\\Project\\CN\\CN")
+
+def get_data(data):
+	raw_data = open(data, "r+", encoding="utf-8")	# r+ is Special read and write mode, which is used to handle both actions when working with a file
+	read = raw_data.read()
+	print (read)
+
+directory1 = "C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/dev.in"
+
+get_data(directory1)
