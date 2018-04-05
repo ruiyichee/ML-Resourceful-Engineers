60b504a6a83bdd0158e042cb42b57026b90c5c39
Rui\ruiyicx <ruiyi_chee@mymail.sutd.edu.sg>
2018-04-05 22:45:17 +0800

    Changed to dictionary for trial and test on ML part 2

--- ML_Proj_Part2.py
+++ ML_Proj_Part2.py
@@ -7,37 +7,41 @@
 #os.chdir("C:\\Users\\ruiyicx\\Documents\\SUTD Subjects\\ESD Term 7\\01.112 Machine Learning\\Project\\CN\\CN")
 
 def get_data(data):
-	dictionary = {}
-	raw_data = open(data, 'r', encoding= "utf8")	# r+ is Special read and write mode, which is used to handle both actions when working with a file
+	raw_data = open(data, 'r+', encoding= "utf8")	# r+ is Special read and write mode, which is used to handle both actions when working with a file
 	d = raw_data.read()
-	# print (d)
-	words = d.split('/n')
-	for words2 in words:
-		words3 = words2.split('\n')
-		words4 = words2.rsplit()
+	pairs = d.split('/n')
+	for words in pairs:
+		words2 = words.split('\n')
+		words3 = words.rsplit()
 		# The words are split in the list format.
-	# print(words3)
+	print(words3)
 
 	# Create a nested list, with the words in the first nested list, and the tags in the second nested list
 	# [[words], [tags]]
-	lst = [[] for x in range(2)]
-	for w in words4:
+	# lst = [[] for x in range(2)]
+
+	# Create a dictionary for emission count and tag count
+	emission_count = {} 
+	tag_count = {}
+	for w in words3:
 		if w == 'O' or w == 'B-neutral' or w == 'B-negative' or w == 'B-positive' or w == 'I-neutral' or w == 'I-negative' or w == 'I-positive':
-			lst[1].append(w)
+			if w not in tag_count:
+				tag_count[w] = 1
+			else:
+				tag_count[w] += 1
 		else:
-			lst[0].append(w)
+			if w not in emission_count:
+				emission_count[w] = 1
+			else:
+				emission_count[w] += 1
 
-	# Create a list, with the total count of tags
-
-	# Create a list, that counts the occurrence of words by tags
-
-	countu = words4.count('O')
+	countu = words3.count('O')
 	print(countu)
 	# print(lst)
 	# for item in 
-
-		
-	# print(dictionary)
+	
+	print(emission_count)
+	print(tag_count)
 
 #EN
 dirEN_train = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/train'
