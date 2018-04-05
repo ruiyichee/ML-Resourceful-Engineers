ae1226ea318651c0bd910160ef3f1cc73b1d3cd3
Rui\ruiyicx <ruiyi_chee@mymail.sutd.edu.sg>
2018-04-05 17:28:42 +0800

    Updated ML_Proj_Part2 using a nested list Draft Part 2 contain code for dictionary

--- Draft Part 2.py
+++ Draft Part 2.py
@@ -15,30 +15,20 @@ def get_data(data):
 	for words2 in words:
 		words3 = words2.split('\n')
 		words4 = words2.rsplit()
-		# The words are split in the list format.
-	# print(words3)
+	print(words3)
 
-	# Create a nested list, with the words in the first nested list, and the tags in the second nested list
-	# [[words], [tags]]
-	lst = [[] for x in range(2)]
-	for w in words4:
-		if w == 'O' or w == 'B-neutral' or w == 'B-negative' or w == 'B-positive' or w == 'I-neutral' or w == 'I-negative' or w == 'I-positive':
-			lst[1].append(w)
+	# Develop a dictionary, taking the occurrences of the key as the value
+	for w in words3:
+		if w not in dictionary:
+			dictionary[w] = 1
 		else:
-			lst[0].append(w)
-
-	# Create a list, with the total count of tags
-
-	# Create a list, that counts the occurrence of words by tags
+			dictionary[w] += 1
 
 	countu = words4.count('O')
 	print(countu)
-	# print(lst)
-	# for item in 
 
 		
 	# print(dictionary)
-
 #EN
 dirEN_train = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/train'
 dirEN_in = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/dev.in'
