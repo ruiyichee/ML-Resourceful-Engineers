59de1c92f4885f4eb7790500d04edae325f91836
Rui\ruiyicx <ruiyi_chee@mymail.sutd.edu.sg>
2018-04-13 18:05:04 +0800

    Working part 2

--- ML_Proj_Part2.py
+++ ML_Proj_Part2.py
@@ -2,14 +2,11 @@
 
 # from datetime import datetime
 # start_time = datetime.now()
-
-# import os
-# os.chdir('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project')
+import os
 
 ggdict = {}
 nesteddict = {"B-positive": 0, "B-negative": 0, "B-neutral": 0, "I-positive": 0, "I-negative": 0, "I-neutral": 0,
               "O": 0}
-
 
 def get_data(data):
     raw_data = open(data, 'r',
@@ -18,92 +15,9 @@ def get_data(data):
     words = d.split('/n')
     return words
 
-
-def emission_par(words):
-    global ggdict
-    global nesteddict
-
-    totalcountBpos = 0
-    totalcountBneg = 0
-    totalcountBneu = 0
-    totalcountIpos = 0
-    totalcountIneg = 0
-    totalcountIneu = 0
-    totalcountO = 0
-    for words2 in words:
-        globaldict = {}
-        words3 = words2.split('\n')  ##words3 is a list of pairs of words and their labels
-        for i in range(len(words3)):
-            spt = words3[i].split(" ")
-            if len(spt) > 3 or len(spt) < 2:
-                continue
-            # print(spt)
-            key1 = spt[0]
-            label1 = spt[1]
-            if key1 not in globaldict:
-                globaldict[key1] = nesteddict
-                globaldict[key1][label1] += 1
-            nesteddict = {"B-positive": 0, "B-negative": 0, "B-neutral": 0, "I-positive": 0, "I-negative": 0,
-                          "I-neutral": 0, "O": 0}
-
-            if label1 == 'B-positive':
-                totalcountBpos += 1
-            elif label1 == 'B-negative':
-                totalcountBneg += 1
-            elif label1 == 'B-neutral':
-                totalcountBneu += 1
-            elif label1 == 'I-positive':
-                totalcountIpos += 1
-            elif label1 == 'I-negative':
-                totalcountIneg += 1
-            elif label1 == 'I-neutral':
-                totalcountIneu += 1
-            else:
-                totalcountO += 1
-
-        for k in globaldict:  ##k refers to the words in the smaller global dictionary
-            vdict = globaldict[k]  ##vdict refers to the existing dictionary of label counts for the particular word k
-            if k not in ggdict:  ## if the word k does not exist in the global global dictionary
-                ggdict[k] = vdict  ## update the global global dictionary with the existing dictionary of label counts
-            else:
-                for k1, v in vdict.items():
-                    ggdict[k][k1] += v  ##add the label counts to the global global dictionary
-
-        # Calculating the emission parameters
-        for word, tagdict in ggdict.items():
-            for tag, count in tagdict.items():
-                if tag == 'B-positive':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountBpos
-                elif tag == 'B-negative':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountBneg
-                elif tag == 'B-neutral':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountBneu
-                elif tag == 'I-positive':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountIpos
-                elif tag == 'I-negative':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountIneg
-                elif tag == 'I-neutral':
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountIneu
-                else:
-                    ggdict[word][tag] = ggdict[word][tag] / totalcountO
-        #print(ggdict)
-
-
-# print("ggdict:%s" %ggdict)
-
-# print(totalcountO)
-# print(totalcountBpos)
-# print(totalcountBneg)
-# print(totalcountBneu)
-# print(totalcountIpos)
-# print(totalcountIneg)
-# print(totalcountIneu)
-
 def modified_test(trainfile, testfile):
     raw_train = open(trainfile, 'r+',
                      encoding="utf8")  # r+ is Special read and write mode, which is used to handle both actions when working with a file
-    # d_train = raw_train.read()
-    # words = d_train.split('/n')
 
     words_in_train = []
     for words2 in raw_train:
@@ -120,31 +34,26 @@ def modified_test(trainfile, testfile):
     raw_test = open(testfile, 'r+',
                     encoding="utf8")  # r+ is Special read and write mode, which is used to handle both actions when working with a file
     words_in_test = []
-    for words2 in raw_test:
-        words3 = words2.rsplit()
-        # print(words3)
-        for i in range(len(words3)):
-            key = words3[i]
-            words_in_test.append(key)
+    read_lines = raw_test.read()
+    lines = read_lines.split('\n')
+    for words in lines:
+        try:
+            words_in_test.append(words)
+        except:
+            words_in_test.append('')
     modified_test = words_in_test
 
     kcount = 0
     for x in range(len(words_in_test)):
-        if words_in_test[x] not in words_in_train:
+        if words_in_test[x] == '':
+            modified_test[x] = modified_test[x]
+        elif words_in_test[x] not in words_in_train:
             modified_test[x] = '#UNK#'
             kcount += 1
-    # print(modified_test)
-    # print(kcount)
-
-    f = open("modified_file.txt", "w+", encoding="utf8")
-    for item in range(len(modified_test)):
-        word = modified_test[item]
-        f.write("%s\n" % word)
-    f.close()
 
     return modified_test, kcount
 
-def modified_emission_par(words,kcount):
+def emission_par(words,kcount):
     global ggdict
     global nesteddict
 
@@ -211,86 +120,97 @@ def modified_emission_par(words,kcount):
                     ggdict[word][tag] = ggdict[word][tag] / (totalcountIneu +kcount)
                 else:
                     ggdict[word][tag] = ggdict[word][tag] / (totalcountO +kcount)
-        # print(ggdict)
+        # print("ggdict: %s" %ggdict)
     return ggdict
 
 def sentiment_analysis(test_file, emission_params):
   #test_file is dev.in file , output_file is the output of the maximized probabilities of the tags for the words in dev.in, emission_params is the ggdict
-    output = {}
-    input_file = get_data(test_file)
+    testwords = []
+    tag = []
     counter = 0
-    for line in input_file:
-        # print(line)
-        word = line.split()
-        # print(word)
-        for w in word:
-            if w in emission_params.keys():
-                counter += 1
-                # print('YES')
-                print(counter)
-                tagdict = emission_params[w]
-                # print(tagdict)
-                output[w] = max(tagdict,key=tagdict.get)
-                UNKtag = max(emission_params['#UNK#'], key= emission_params['#UNK#'].get)
-            else:
-                output[w] = "#UNK#" #for missing words, tag = #UNK#
-    print(output)
-    f = open("dev.p2.out", "w+", encoding="utf8")
-    for key,value in output.items():
-        if value == "#UNK#":
-            f.write("%s %s\n" % ("#UNK#", UNKtag))
+
+    for w in test_file:
+        counter += 1
+        if w in emission_params.keys():
+            tagdict = emission_params[w]
+            testwords.append(w)
+            tag.append(max(tagdict,key=tagdict.get))
+        elif w == "#UNK#":
+            testwords.append("#UNK#")
+            UNKtag = max(emission_params['#UNK#'], key= emission_params['#UNK#'].get)
+            tag.append(UNKtag)
         else:
-            f.write("%s %s\n" % (key,value))
+            testwords.append("")
+            tag.append("")
+    #print(counter)
+
+    f = open("dev.p2.out", "w+", encoding="utf8")
+    for i in range(0,counter):
+        f.write("%s %s\n" % (testwords[i],tag[i]))
     f.close()
+    print('DONE')
+
+    # for key,value in output.items():
+    #     if value == "#UNK#":
+    #         f.write("%s %s\n" % ("#UNK#", UNKtag))
+    #     else:
+    #         f.write("%s %s\n" % (key,value))
+    # f.close()
+    # print('DONE')
 
 # EN
 # RUIYI
+os.chdir('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN')
 dirEN_train = ('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/train')
 dirEN_in = ('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/dev.in')
 dirEN_out = ('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/EN/EN/dev.out')
 dirEN_test = ('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/modified_file.txt')
+## EN
+modified_test_EN, kcount_EN = modified_test(dirEN_train, dirEN_in)
+emission_param_EN = emission_par(get_data(dirEN_train),kcount_EN)
+sentiment_analysis(modified_test_EN, emission_param_EN)
+print("EN DONE")
+
 # CN
+os.chdir('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN')
 dirCN_train = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/train'
 dirCN_in = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/dev.in'
 dirCN_out = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/dev.out'
 dirCN_test = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/CN/CN/modified_file.txt'
+## CN
+modified_test_CN, kcount_CN = modified_test(dirCN_train, dirCN_in)
+emission_param_CN = emission_par(get_data(dirCN_train),kcount_CN)
+sentiment_analysis(modified_test_CN, emission_param_CN)
+print("CN DONE")
+
 # ES
+os.chdir('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/ES/ES')
 dirES_train = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/ES/ES/train'
 dirES_in = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/ES/ES/dev.in'
 dirES_out = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/ES/ES/dev.out'
 dirES_test = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/ES/ES/modified_file.txt'
+## ES
+modified_test_ES, kcount_ES = modified_test(dirES_train, dirES_in)
+emission_param_ES = emission_par(get_data(dirES_train),kcount_ES)
+sentiment_analysis(modified_test_ES, emission_param_ES)
+print("ES DONE")
+
 # RU
+os.chdir('C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/RU/RU')
 dirRU_train = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/RU/RU/train'
 dirRU_in = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/RU/RU/dev.in'
 dirRU_out = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/RU/RU/dev.out'
 dirRU_test = 'C:/Users/ruiyicx/Documents/SUTD Subjects/ESD Term 7/01.112 Machine Learning/Project/RU/RU/modified_file.txt'
+## RU
+modified_test_RU, kcount_RU = modified_test(dirRU_train, dirRU_in)
+emission_param_RU = emission_par(get_data(dirRU_train),kcount_RU)
+sentiment_analysis(modified_test_RU, emission_param_RU)
+print("RU DONE")
 
 # REGINA
 # dirEN_train = ('C:/Users/Regina/Documents/SUTD/ESD Term 5/Machine Learning/Project/EN/EN/train')
 # dirEN_in = ('C:/Users/Regina/Documents/SUTD/ESD Term 5/Machine Learning/Project/EN/EN/dev.in')
 # dirEN_out = ('C:/Users/Regina/Documents/SUTD/ESD Term 5/Machine Learning/Project/EN/EN/dev.out')
-
-## EN
-# emission_par(get_data(dirEN_train))
-# modified_test, kcount = modified_test(dirEN_train, dirEN_in)
-# emission_param = modified_emission_par(get_data(dirEN_train),kcount)
-# #print(emission_param)
-# sentiment_analysis(dirEN_in, emission_param)
-
-## CN
-# modified_test, kcount = modified_test(dirCN_train, dirCN_in)
-# emission_param = modified_emission_par(get_data(dirCN_train),kcount)
-# sentiment_analysis(dirCN_in, emission_param)
-
-## ES
-# modified_test, kcount = modified_test(dirES_train, dirES_in)
-# emission_param = modified_emission_par(get_data(dirES_train),kcount)
-# sentiment_analysis(dirES_in, emission_param)
-
-## RU
-# modified_test, kcount = modified_test(dirRU_train, dirRU_in)
-# emission_param = modified_emission_par(get_data(dirRU_train),kcount)
-# sentiment_analysis(dirRU_in, emission_param)
 
 # end_time = datetime.now()
 # print('Duration: {}'.format(end_time - start_time))
